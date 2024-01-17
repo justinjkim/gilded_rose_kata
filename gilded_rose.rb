1afae75072c1350bc266559011870f94e8a6a305
justinjkim <jjkim928@gmail.com>
2024-01-17 10:35:04 -0500

    1/17/24: remove old comments

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,5 +1,3 @@
-
-# new refactor
 def update_quality(items)
   items.each do |item|
     case item.name
@@ -36,56 +34,6 @@ def update_quality(items)
     end
   end
 end
-
-
-# original
-# def update_quality(items)
-#   items.each do |item|
-#     if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-#       if item.quality > 0
-#         if item.name != 'Sulfuras, Hand of Ragnaros'
-#           item.quality -= 1
-#         end
-#       end
-#     else
-#       if item.quality < 50
-#         item.quality += 1
-#         if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-#           if item.sell_in < 11
-#             if item.quality < 50
-#               item.quality += 1
-#             end
-#           end
-#           if item.sell_in < 6
-#             if item.quality < 50
-#               item.quality += 1
-#             end
-#           end
-#         end
-#       end
-#     end
-#     if item.name != 'Sulfuras, Hand of Ragnaros'
-#       item.sell_in -= 1
-#     end
-#     if item.sell_in < 0
-#       if item.name != "Aged Brie"
-#         if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-#           if item.quality > 0
-#             if item.name != 'Sulfuras, Hand of Ragnaros'
-#               item.quality -= 1
-#             end
-#           end
-#         else
-#           item.quality = item.quality - item.quality
-#         end
-#       else
-#         if item.quality < 50
-#           item.quality += 1
-#         end
-#       end
-#     end
-#   end
-# end
 
 # DO NOT CHANGE THINGS BELOW -----------------------------------------
 
