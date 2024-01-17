23152be0d3c80657fbb9e7534791244ce244c754
justinjkim <jjkim928@gmail.com>
2024-01-17 10:32:10 -0500

    1/17/24: first pass at refactor
    I kept the original logic but commented it out, and did a first pass at
    a refactor. It's still not very pretty, but at least it looks better
    than the original wicked conditional tree. I think reading the slightly
    refactored code is a lot easier on the eyes, and gives you a clearer
    idea of what happens to the item.

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,50 +1,91 @@
+
+# new refactor
 def update_quality(items)
   items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
-      end
-    else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
+    case item.name
+    when "NORMAL ITEM"
       item.sell_in -= 1
-    end
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
-      else
-        if item.quality < 50
-          item.quality += 1
-        end
+      return if item.quality == 0
+      item.quality -= 1
+      item.quality -= 1 if item.sell_in <= 0
+    when "Aged Brie"
+      item.sell_in -= 1
+      item.quality += 1 if item.quality < 50
+      item.quality += 1 if item.sell_in <= 0 && item.quality < 50
+    when "Sulfuras, Hand of Ragnaros"
+      return
+    when "Backstage passes to a TAFKAL80ETC concert"
+      if item.sell_in <= 0
+        item.quality = 0
+        item.sell_in -= 1
+        return
       end
+
+      if item.sell_in <= 5
+        item.quality += 1 if item.quality < 50
+        item.quality += 1 if item.quality < 50
+        item.quality += 1 if item.quality < 50
+      elsif item.sell_in <= 10
+        item.quality += 1 if item.quality < 50
+        item.quality += 1 if item.quality < 50
+      else
+        item.quality += 1 if item.quality < 50
+      end
+
+      item.sell_in -= 1
     end
   end
 end
+
+
+# original
+# def update_quality(items)
+#   items.each do |item|
+#     if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
+#       if item.quality > 0
+#         if item.name != 'Sulfuras, Hand of Ragnaros'
+#           item.quality -= 1
+#         end
+#       end
+#     else
+#       if item.quality < 50
+#         item.quality += 1
+#         if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+#           if item.sell_in < 11
+#             if item.quality < 50
+#               item.quality += 1
+#             end
+#           end
+#           if item.sell_in < 6
+#             if item.quality < 50
+#               item.quality += 1
+#             end
+#           end
+#         end
+#       end
+#     end
+#     if item.name != 'Sulfuras, Hand of Ragnaros'
+#       item.sell_in -= 1
+#     end
+#     if item.sell_in < 0
+#       if item.name != "Aged Brie"
+#         if item.name != 'Backstage passes to a TAFKAL80ETC concert'
+#           if item.quality > 0
+#             if item.name != 'Sulfuras, Hand of Ragnaros'
+#               item.quality -= 1
+#             end
+#           end
+#         else
+#           item.quality = item.quality - item.quality
+#         end
+#       else
+#         if item.quality < 50
+#           item.quality += 1
+#         end
+#       end
+#     end
+#   end
+# end
 
 # DO NOT CHANGE THINGS BELOW -----------------------------------------
 
